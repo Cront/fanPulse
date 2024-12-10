dfadf4ffe7f1e04be7b0f28df20e9a1814c2cad0
Syed A <63989020+Cront@users.noreply.github.com>
2024-12-10 06:19:31 -0800

    Fixed not recognizing database issue

--- backend/config.py
+++ backend/config.py
@@ -13,10 +13,10 @@ CORS(app)
 # password hashing
 bcrypt = Bcrypt(app)
 
-db = SQLAlchemy(app)
-
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+db = SQLAlchemy(app)
